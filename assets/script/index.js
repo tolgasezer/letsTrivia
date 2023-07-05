29ad03deb07928eec9926f2e3601184155966047
tolgaszr <tolgaszr@gmail.com>
2023-07-05 15:24:39 +0300

    score fixed

--- assets/script/index.js
+++ assets/script/index.js
@@ -21,7 +21,7 @@ let availableQuestions = [];
 let questions = [];
 //let prevQuestions = [];
 let selections = [];
-let revSelections = selections.reverse();
+
 console.log(selections) // selections her prev tiklamasinda ters
 // index uzerinden ilerlemeli ++
 // geri butonu olmali ++
@@ -147,7 +147,14 @@ const nextQuestion = () =>{
     const selected = document.querySelector('.selected');
     selections.push(selected.innerText);
   };
-  console.log(selections);
+  if(selections[questionCounter]===currentQuestion.choiceText[currentQuestion.answer]){
+    console.log('bildin')
+    score++;
+  }else{
+    console.log('yanlis')
+  }
+  //console.log(selections);
+  //console.log(currentQuestion.choiceText[currentQuestion.answer]);
   questionCounter++;
   showQuestion();
   restorePreviousSelection();
@@ -192,6 +199,7 @@ const restorePreviousSelection = () => {
 const endGame = (questionLength) => {
   nextBtn.classList.add('hidden');
   restartBtn.classList.remove('hidden');
+  prevBtn.classList.add('hidden');
   question.innerText = `Your Score is: ${score * CORRECT_BONUS}/${questionLength * CORRECT_BONUS}`;
   choiceContainer.style.display = 'none';
   return;
@@ -203,53 +211,9 @@ const restartQuiz = () => {
   startGame();
 };
 
-//Buradan her soru icin dogru yanlis kontrol edip cerceve ekleyecegim. showQuestion icin de index ve prev buton eklemem gerekli
-// const selectedHandler = () =>{
-//   const selected = document.querySelector('.selected');
-//   prevQuestions.push(currentQuestion);
-//   prevAnswer.push(selected);
-//   //prevQuestions.push(currentQuestion);
-//   console.log(prevQuestions);
-//   if (selected.innerText == currentQuestion.choiceText[currentQuestion.answer]) {
-//     score++;
-//   }
-//   showQuestion();
-  
-// };
-
-
-
-// const getPrevQuestionWithAnswer = () => {
-  
-//   questionCounter--;
-//   const prevQues = prevQuestions[questionCounter-1];
-//   const prevAns =prevAnswer[questionCounter-1];
-//   //const prevAnswers = prevAnswer[questionCounter]
-//   //const targetChild = prevQues.answer;
-//   //console.log(targetChild);
-//   question.innerText = prevQues.question;
-//   choiceContainer.innerText = '';
-  
-//   //questionCounter++;
-  
-//   for (let i = 0; i < prevQues.choiceText.length; i++) {
-//     const choiceText = prevQues.choiceText[i];
-//     createChoiceElement(choiceText);
-//     if (choiceText == prevAns){
-//       choiceText.classList.add('selected');
-//     }
-    
-//   };
-  
-//   //targetChild.classList.add('selected');
-//   //console.log(prevAnswers)
-//   // console.log(prevQues.question);
-//   // console.log(prevQues.choiceText);
-//   // console.log(prevQues.answer)
-
-// };
 nextBtn.addEventListener('click', nextQuestion);
 restartBtn.addEventListener('click', restartQuiz);
 prevBtn.addEventListener('click', prevQuestion);
 
 initGame();
+
