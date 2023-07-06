8ca9739b2437900b4e0fa760754e7a1f46f4b13d
tolgaszr <tolgaszr@gmail.com>
2023-07-06 15:04:31 +0300

    background change logic needs to fixed

--- assets/script/index.js
+++ assets/script/index.js
@@ -1,17 +1,17 @@
-const question = document.getElementById('question');
-const choiceContainer = document.getElementById('choice-container');
-const choices = Array.from(document.getElementsByClassName('choice-text'));
-const nextBtn = document.getElementById('nxt-btn');
-const restartBtn = document.getElementById('restart-btn');
-const loader = document.getElementById('loader');
-const game = document.getElementById('containerC');
-const prevBtn = document.getElementById('prev-btn');
-game.classList.add('hidden');
-game.classList.add('justify-center');
+const question = document.getElementById("question");
+const choiceContainer = document.getElementById("choice-container");
+const choices = Array.from(document.getElementsByClassName("choice-text"));
+const nextBtn = document.getElementById("nxt-btn");
+const restartBtn = document.getElementById("restart-btn");
+const loader = document.getElementById("loader");
+const game = document.getElementById("containerC");
+const prevBtn = document.getElementById("prev-btn");
+game.classList.add("hidden");
+game.classList.add("justify-center");
 
 const CORRECT_BONUS = 10;
-const difficulty = sessionStorage.getItem('difficulty');
-const numberOfQuestions = sessionStorage.getItem('numberOfQuestions');
+const difficulty = sessionStorage.getItem("difficulty");
+const numberOfQuestions = sessionStorage.getItem("numberOfQuestions");
 let currentQuestion = {};
 let score = 0;
 let questionCounter = 0;
@@ -22,7 +22,7 @@ let questions = [];
 //let prevQuestions = [];
 let selections = [];
 
-console.log(selections) // selections her prev tiklamasinda ters
+console.log(selections); // selections her prev tiklamasinda ters
 // index uzerinden ilerlemeli ++
 // geri butonu olmali ++
 // cevap verildikten sonra dogru cevap yesil, yanlis cevaplar kirmizi, kullanicinin cevabina border --
@@ -35,8 +35,8 @@ const initGame = async () => {
   availableQuestions = [...questions];
   showQuestion();
   disableNextButton();
-  game.classList.remove('hidden');
-  loader.classList.add('hidden');
+  game.classList.remove("hidden");
+  loader.classList.add("hidden");
 };
 
 const getQuestionsAsync = async () => {
@@ -77,92 +77,95 @@ const shuffle = (array) => {
   return array;
 };
 
-
-const createChoiceElement = (choiceText) =>{
-  const newChoice = document.createElement('div');
+const createChoiceElement = (choiceText) => {
+  const newChoice = document.createElement("div");
   const choiceTextNode = document.createTextNode(choiceText);
   newChoice.appendChild(choiceTextNode);
   choiceContainer.appendChild(newChoice);
-  newChoice.classList.add('choice-container');
-  
-  newChoice.addEventListener('click', () => {
+  newChoice.classList.add("choice-container");
+
+  newChoice.addEventListener("click", () => {
     selectChoice(newChoice);
   });
-  
 };
 
-//burada check edip background vererek ilerleyecegim unutma
-const checkAnswer = () => {
-  if(choiceContainer.contains('.selected')){
-  if(choice.innerText === currentQuestion.choiceText[currentQuestion.answer]){
-    choice.classList.add('bg-success');
-  }else{
-    choice.classList.add('bg-danger');
+//burada check edip background vererek ilerleyecegim unutma // felaket sacmaladim. Logic belirsiz
+const checkAnswer = (choiceElement) => {
+ 
+  if (choiceElement.classList.contains(".selected")) {
+    if (
+      choice.innerText === currentQuestion.choiceText[currentQuestion.answer]
+    ) {
+      choice.classList.add("bg-success");
+    } else {
+      choice.classList.add("bg-danger");
+    }
   }
-}
 };
-const selectChoice = (choiceElement) =>{
-  const allChoices = document.querySelectorAll('.choice-container');
-  allChoices.forEach((choice)=>{
-    choice.setAttribute('disabled', '');
-  })
-  if(!selections[questionCounter]){
-  const allChoices = document.querySelectorAll('.choice-container');
+const selectChoice = (choiceElement) => {
+  const allChoices = document.querySelectorAll(".choice-container");
+  allChoices.forEach((choice) => {
+    choice.setAttribute("disabled", "");
+  });
+  if (!selections[questionCounter]) {
+    const allChoices = document.querySelectorAll(".choice-container");
     allChoices.forEach((choice) => {
-      choice.classList.remove('selected');
+      choice.classList.remove("selected");
     });
-    choiceElement.classList.add('selected');
+    choiceElement.classList.add("selected");
+    checkAnswer(choiceElement);
+
     enableNextButton();
-  };
-    
+  }
 };
 
 const disableNextButton = () => {
-  nextBtn.setAttribute('disabled', '');
+  nextBtn.setAttribute("disabled", "");
 };
 
 const enableNextButton = () => {
-  nextBtn.removeAttribute('disabled');
+  nextBtn.removeAttribute("disabled");
 };
-
 
 const showQuestion = () => {
   const availableQuestionsLength = availableQuestions.length;
-  
 
   if (questionCounter >= availableQuestionsLength) {
     endGame(availableQuestionsLength);
     return;
   }
 
-  choiceContainer.innerText = '';
+  choiceContainer.innerText = "";
   currentQuestion = availableQuestions[questionCounter];
   question.innerText = currentQuestion.question;
-  
-  for (let i = 0; i < currentQuestion.choiceText.length; i++) {  
+
+  for (let i = 0; i < currentQuestion.choiceText.length; i++) {
     createChoiceElement(currentQuestion.choiceText[i]);
-  };
-  
+  }
 };
 // son tiklamada next button disabled olmuyor
-const nextQuestion = () =>{
-  prevBtn.classList.remove('hidden');
+const nextQuestion = () => {
+  prevBtn.classList.remove("hidden");
 
-
-  if (previousCounter > 0){
+  if (previousCounter > 0) {
     previousCounter--;
-
-  } 
-  if (previousCounter === 0 && selections.length === questionCounter){ // ileri geri icin tekrar eklemeleri buradan engelledim.
+  }
+  if (previousCounter === 0 && selections.length === questionCounter) {
+    // ileri geri icin tekrar eklemeleri buradan engelledim.
     disableNextButton();
-    const selected = document.querySelector('.selected');
+    const selected = document.querySelector(".selected");
     selections.push(selected.innerText);
-  };
-  if(selections[questionCounter]===currentQuestion.choiceText[currentQuestion.answer] && previousCounter === 0){ // geri dondugumde skoru degistirmemesi icin ek kontrol
-    console.log('bildin')
+  }
+  if (
+    selections[questionCounter] ===
+      currentQuestion.choiceText[currentQuestion.answer] &&
+    previousCounter === 0
+  ) {
+    // geri dondugumde skoru degistirmemesi icin ek kontrol
+    console.log("bildin");
     score++;
-  }else{
-    console.log('yanlis')
+  } else {
+    console.log("yanlis");
   }
   //console.log(selections);
   //console.log(currentQuestion.choiceText[currentQuestion.answer]);
@@ -172,67 +175,60 @@ const nextQuestion = () =>{
 };
 
 const prevQuestion = () => {
-    
   if (questionCounter > 0) {
-    
     questionCounter--;
     enableNextButton();
-    
+
     //const selectedChoice = revSelections[previousCounter];
     //console.log(currentQuestion.choiceText)
     showQuestion();
-    
-    
+
     // Saklanmış seçimi geri yükle
     restorePreviousSelection();
-    
   }
-  if(questionCounter === 0) {
-    prevBtn.classList.add('hidden');
+  if (questionCounter === 0) {
+    prevBtn.classList.add("hidden");
   }
   console.log(selections);
   previousCounter++;
 };
 
 const restorePreviousSelection = () => {
-
   const selectedChoice = selections[questionCounter];
-  const choices = document.querySelectorAll('.choice-container');
-  choices.forEach(choice => {
-    choice.classList.remove('selected');
+  const choices = document.querySelectorAll(".choice-container");
+  choices.forEach((choice) => {
+    choice.classList.remove("selected");
     if (choice.innerText === selectedChoice) {
-      choice.classList.add('selected');
-      
+      choice.classList.add("selected");
     }
   });
 };
 
 const endGame = (questionLength) => {
-  
-  nextBtn.classList.add('hidden');
-  restartBtn.classList.remove('hidden');
+  nextBtn.classList.add("hidden");
+  restartBtn.classList.remove("hidden");
   //prevBtn.classList.add('hidden');
-  prevBtn.addEventListener('click', ()=>{
+  prevBtn.addEventListener("click", () => {
     showQuestion();
     restorePreviousSelection();
-    choiceContainer.style.display = 'block';
-    nextBtn.classList.remove('hidden');
-    
+    choiceContainer.style.display = "block";
+    nextBtn.classList.remove("hidden");
   });
-  question.innerText = `Your Score is: ${score * CORRECT_BONUS}/${questionLength * CORRECT_BONUS}`;
-  choiceContainer.style.display = 'none';
+  question.innerText = `Your Score is: ${score * CORRECT_BONUS}/${
+    questionLength * CORRECT_BONUS
+  }`;
+  choiceContainer.style.display = "none";
   return;
 };
 
 const restartQuiz = () => {
-  window.location.assign('./index.html');
+  window.location.assign("./index.html");
 
   startGame();
 };
 
-nextBtn.addEventListener('click', nextQuestion);
-restartBtn.addEventListener('click', restartQuiz);
-prevBtn.addEventListener('click', prevQuestion);
+nextBtn.addEventListener("click", nextQuestion);
+restartBtn.addEventListener("click", restartQuiz);
+prevBtn.addEventListener("click", prevQuestion);
 
 initGame();
-
